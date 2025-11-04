472d2863671ef44c7f6d9fd430e5a82edb736ed1
Irfan aslam <irfanaslamo700@gmail.com>
2025-11-04 20:09:49 +0530

    Simplify CORS configuration to allow all origins

--- index.js
+++ index.js
@@ -8,21 +8,10 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT;
-const allowedOrigins = [
-  "https://prot-na73.vercel.app",
-  "https://prot-na73-git-main-irfan-aslams-projects-8574b73b.vercel.app",
-  "https://prot-na73-gsnwd776j-irfan-aslams-projects-8574b73b.vercel.app"
-  
-];
+
 
 app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
+  origin: true,
   credentials: true,
 }));
 
